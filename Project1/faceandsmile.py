9a7975f5b68aff2b845723017f7a98fb9d599f0a
Lacy, Connor <lacyconnor1409@gmail.com>
2019-11-11 11:24:02 -0500

    detecting face and smile

---
+++ Project1/faceandsmile.py
@@ -0,0 +1,43 @@
+import picamera
+import numpy as np
+import cv2 as cv
+import IPython
+import io
+import time
+from PIL import Image
+
+if __name__ == "__main__":
+
+    face_cascade = cv.CascadeClassifier('haarcascade_frontalface_alt.xml')
+    smile_cascade = cv.CascadeClassifier('haarcascade_smile.xml')
+
+    #initialize and start realtime video capture
+    cam = cv.VideoCapture(0)
+    cam.set(3,640) #width
+    cam.set(4,480) #height
+    #Define min window size to be recognized as a face
+    minW = 0.1*cam.get(3)
+    minH = 0.1*cam.get(4)
+
+    while True:
+        ret, img = cam.read()
+        img = cv.flip(img, -1) #vertical flip
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x,y,w,h) in faces:
+            cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+            roi_gray = gray[y:y + h, x:x + w] 
+            roi_color = img[y:y + h, x:x + w]
+            smiles = smile_cascade.detectMultiScale(roi_gray, 1.8, 20)
+            for(sx,sy,sw,sh) in smiles:
+                cv.rectangle(roi_color,(sx,sy),((sx+sw),(sy+sh)),(255,0,0),2)
+
+
+        cv.imshow('camera', img)
+        k = cv.waitKey(10) & 0xff
+        if k==27:
+            break
+
+    cam.release()
+    cv.destroyAllWindows()
